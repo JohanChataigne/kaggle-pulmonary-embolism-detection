e86ea61a32f2da1fe2d65786ab640f3c711f4fc4
JohanChataigne <Johan.Chataigner@enseirb-matmeca.fr>
2021-01-23 14:28:43 +0100

    fix evaluation issues

--- model.py
+++ model.py
@@ -1,13 +1,19 @@
 import torch
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
-def train(model, num_epoch, criterion, data_loader, optimizer, device):
+
+def train(model, num_epoch, criterion, data_loader, optimizer, device, filename):
 
     step_count = len(data_loader)
+    losses = list()
 
     print("Training begins")
     
     for epoch in range(num_epoch):
+        
+        epoch_loss = 0
+        
         for i, sample in enumerate(data_loader):
 
             image = sample['image'].to(device, dtype=torch.float)
@@ -22,18 +28,23 @@ def train(model, num_epoch, criterion, data_loader, optimizer, device):
 
             # Compute loss
             loss = criterion(pred, target)
-
+            epoch_loss += loss
             # Backprop
             loss.backward()
             optimizer.step()
 
             # Debug
-            if((i+1) % (step_count/10) == 0):
+            if((i+1) % int(step_count/10) == 0):
                 print(f"Epoch [{epoch + 1}/{num_epoch}]" f", step [{i + 1}/{step_count}]" f", loss: {loss.item():.4f}")
+            
+        losses.append(epoch_loss)
                 
     print("Finished training")
     
-    return model
+    
+    torch.save(model.state_dict(), filename)
+    
+    return model, losses
         
         
 def evaluate(model, data_loader, device):
@@ -42,16 +53,32 @@ def evaluate(model, data_loader, device):
     total = 0
     
     with torch.no_grad():
-        
         for sample in data_loader:
-            
-            images = sample['image'].to(device, dtype=torch.float)
-            targets = sample['target'].to(device)
-            
-            outputs = model(images)
-            
-            _, predicted = torch.max(outputs.data, 1)
-            total += targets.size(0)
-            correct += (predicted == targets).sum().item()
 
-    print(f'Accuracy of the network on the {total} test images: {100 * correct / total:.2f}%')
+            images = sample['image'].to(device, dtype=torch.float)        
+            targets = sample['target'].to(device)
+
+            outputs = model(images)
+            class_0 = torch.full((targets.size(0),1), 0.5).to(device)
+            outputs = torch.cat((class_0, outputs), 1)
+
+            _, pred = torch.max(outputs.data, 1)
+
+            total += targets.size(0)
+            correct += (pred == targets).sum().item()
+            
+            
+    return 100 * correct / total
+
+    
+    
+def plot_loss(losses, num_epoch, step_count, filename):
+    
+    epochs = list(range(1, num_epoch+1))
+    mean_losses = list(map(lambda x: x/step_count, losses))
+    plt.plot(epochs, mean_losses)
+    plt.xlabel('Epoch')
+    plt.ylabel('Average loss')
+    plt.grid()
+    plt.savefig(filename)
+    plt.show()
